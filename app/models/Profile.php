cf9df3f408afd1d69e8f615d6bec8ca940b31e9a
Hidemasa Nakada <wingnakada@gmail.com>
2013-07-02 09:40:16 +0900

    Profileモデルの修正

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -9,5 +9,26 @@ class Profile extends Eloquent{
 */
 	public function user(){
 	return $this->belongsTo('User');
-}
+	}
+	
+/*
+|--------------------------------------------
+| 各項目内の配列を取得する
+|--------------------------------------------
+*/
+	static public function item($item){
+		//ログイン中のユーザーの最新情報を取得
+		$query=Profile::where('user_id','=',Auth::user()->id)
+										->orderBy('created_at','desc')
+										->first();
+		//引数に指定した項目がNULLでなければ
+		if($query->$item != null){
+			//アンシリアライズして、配列データを返します。
+			$data=unserialize($query->$item);
+			return $data;
+		//項目がNULLなら
+		}else{
+			return null;
+		}
+	}
 }

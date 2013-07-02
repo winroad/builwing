76d8ddb4d8e9376de40439ed77b537a29847e086
Hidemasa Nakada <wingnakada@gmail.com>
2013-07-02 14:08:58 +0900

    CategoryControllerの追加

---
+++ app/controllers/CategoryController.php
@@ -0,0 +1,42 @@
+<?php
+class CategoryController extends BaseController{
+/*
+|----------------------------------------
+| コンストラクター
+|----------------------------------------
+*/
+ public function __construct(){
+ //adminフィルター
+ $this->beforeFilter('admin');
+ //全POSTにcsrfフィルターの適用
+ $this->beforeFilter('csrf',array('on'=>'post'));
+ }
+ 
+ public function getIndex(){
+	 echo 'getIndexです。';
+ }
+ //新規分類項目の作成
+	public function getCreate(){
+		$data['categories']=Item::all();
+		return View::make('category/create',$data);
+	}
+	public function postCreate(){
+		//受信データ
+		$inputs=Input::only('name','description');
+		//バリルール
+		$rules=array(
+			'name'=>'required|alpha',
+			'description'=>'required',
+			);
+		//バリチェック
+		$val=Validator::make($inputs,$rules);
+		if($val->fails()){
+			return Redirect::back()
+				->withInput()
+				->withErrors($val);
+		}
+		//分類項目の登録
+		Category::create($inputs);
+		return Redirect::intended('admin');
+	}
+}

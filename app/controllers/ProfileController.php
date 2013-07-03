796f4f44f33b21396df707ef79df1d1010a044e3
Hidemasa Nakada <wingnakada@gmail.com>
2013-07-03 15:59:20 +0900

    ProdileControllerの修正

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -27,17 +27,32 @@ class ProfileController extends BaseController{
 | 新規作成ページ
 |----------------------------------------
 */
-	public function getCreate(){
+	public function getCreate($column){
+		//itemsテーブルの未入力データを取得
+		$data['items']=Profile::item($column,true);
+		//配列数が0なら
+		if(count($data['items']) == 0){
+			$message='未入力データはありません';
+		}else{
+			$message=null;
+		}
+		//カテゴリデータを取得
+		$category=Category::where('name','=',$column)->first();
+		//項目名データを取得
+		$data['description']=$category->description;
+		$data['column']=$category->name;
 		//新規作成ページの表示
-		return View::make('profile/create');
+		return View::make('profile/create',$data)
+				->with('message',$message);
 	}
+	
 	public function postCreate(){
 		//POSTデータの取得
-		$inputs=Input::except('id');
+		$inputs=Input::except('id','column','_token');
 		//バリデーションルールの作成
-		$rules=array(
-			'tel'=>'required',
-			);
+		foreach($inputs as $key=>$value):
+			$rules[$key]='required';
+		endforeach;
 		//バリデーション処理
 		$val=Validator::make($inputs,$rules);
 		//バリデーションNGなら
@@ -46,8 +61,27 @@ class ProfileController extends BaseController{
 				->withInput()
 				->withErrors($val);
 		}
+		$column=Input::get('column');
 		//データの新規作成
-		Profile::create($inputs);
+		$profile=Profile::where('user_id','=',Auth::user()->id)->first();
+		//カラム内がNULLなら
+		if($profile->$column == null){
+			//入力値の配列を保存
+			$profile->$column=serialize($inputs);
+			$profile->save();
+		}else{
+		//カラム内がNULLでなければ
+		$a=unserialize($profile->$column);	//元データ
+		$b=$inputs;													//受信データ
+		//2つの配列を併合して一つの配列へ
+		$merge=array_merge($a,$b);
+		//Proileを取得
+		$profile=Profile::where('user_id','=',Auth::user()->id)->first();
+		//シリアライズ処理した配列を入力
+		$profile->$column=serialize($merge);
+		//データ保存
+		$profile->save();
+		}
 		//一覧ページへ移動
 		return Redirect::to('profile/index')
 			->with('message','データを作成しました');
@@ -58,66 +92,18 @@ class ProfileController extends BaseController{
 |----------------------------------------
 */
 	public function getView($id){
-	//データの取得
-	$data['profile']=Profile::find($id);
-	$data['address']=Profile::item('address');
-	$data['body']=Profile::item('body');
-	$data['license']=Profile::item('license');
-	$data['labor']=Profile::item('labor');
-	$data['family']=Profile::item('family');
-	//プロフィール数が0で無ければ
-	if(isset($data['profile'])){
-		return View::make('profile/view',$data);
+		//データの取得
+		$data['profile']=Profile::find($id);
+		$data['address']=Profile::item('address');
+		$data['body']=Profile::item('body');
+		$data['license']=Profile::item('license');
+		$data['labor']=Profile::item('labor');
+		$data['family']=Profile::item('family');
+		//プロフィール数が0で無ければ
+		if(isset($data['profile'])){
+			return View::make('profile/view',$data);
 		}
-		return Redirect::action('ProfileController@getCreate')
-			->with('message','プロフィールを作成してください');
-	}
-/*
-|----------------------------------------
-| 項目作成
-|----------------------------------------
-*/
-	public function getItem($item){
-		$data['item']=$item;
-		$profiles=Profile::orderBy('created_at','desc')->first();
-		return View::make('profile/item',$data);
-	}
-	public function postItem(){
-		//受信データ
-		$inputs=Input::only('name','detail');
-		//バリデーションルール作成
-		$rules=array(
-			'name'=>'required',
-			'detail'=>'required',
-			);
-		//バリデーション処理
-		$val=Validator::make($inputs,$rules);
-		//バリデーションNGなら
-		if($val->fails()){
-			return Redirect::back()
-				->withInput()
-				->withErrors($val);
-		}
-		//データの整理
-		$item=Input::get('item');
-		$name=Input::get('name');
-		$detail=Input::get('detail');
-		$profile=Profile::where('user_id','=',Auth::user()->id)
-			->orderBy('created_at','desc')
-			->first();
-		//項目内が空でなければ
-		if($profile->$item != null){
-		$data=unserialize($profile->$item);
-		$data+=array($name=>$detail);
-		$profile->$item=serialize($data);
-		//項目内が空なら
-		}else{
-		$profile->$item=serialize(array($name=>$detail));
-		}
-		//データの登録
-		$profile->save();
-		//Profile/viewへ移動
-		return Redirect::to('profile/view/'.Auth::user()->id)
-			->with('message','データを作成しました');
+			return Redirect::action('ProfileController@getCreate')
+					->with('message','プロフィールを作成してください');
 	}
 }

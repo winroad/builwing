6d31a2de42e1adadb2c759aeaa4e705e62660400
Hidemasa Nakada <wingnakada@gmail.com>
2013-07-07 17:26:14 +0900

    Messageコントローラの修正
    未読メッセージの削除処理を作成

--- app/controllers/MessageController.php
+++ app/controllers/MessageController.php
@@ -12,24 +12,35 @@ class MessageController extends BaseController{
  $this->beforeFilter('csrf',array('on'=>'post'));
  }
  
+	//個人宛メッセージの取得
 	private function person(){
 		$message=Message::where('recipient_id','=',Auth::user()->id)
 	 		->orderBy('created_at','desc')
 			->paginate();
 		return $message;
 	}
+	//ロールの取得
 	private function role(){
 		$message=Message::where('role_id','>=',Auth::user()->role_id)
 	 		->orderBy('created_at','desc')
 			->paginate();
 		return $message;
 	}
+	//ログインユーザーのロール宛てメッセージの取得
 	private function owner(){
 		$message=Message::where('recipient_id','=',Auth::user()->id)
 			->orWhere('role_id','>=',Auth::user()->role_id)
 	 		->orderBy('created_at','desc')
 			->paginate();
-		return $message;
+		if(isset($message)){
+			return $message;
+		}
+		return null;
+	}
+	//未読メッセージの取得
+	private function unread(){
+		$work=Work::where('user_id',Auth::user()->id);
+		return $work;
 	}
 /*
 |------------------------------------
@@ -37,7 +48,7 @@ class MessageController extends BaseController{
 |------------------------------------
 */
 	public function getIndex(){
-		$data['messages']=$this->owner();
+		$data['messages']=$this->person();
 		return View::make('message/index',$data);
 	}	
 	
@@ -80,25 +91,59 @@ class MessageController extends BaseController{
 
 	/********************************
 	 * 未読処理
-	 * laborテーブルのmessage項目に配列保存
+	 * workテーブルのmessage項目に配列保存
 	 ********************************/
 		
 		//個人宛メッセージなら
 		if(isset($inputs['recipient_id'])){
+			//$old=array();
+			//$new=array();
 			//受信メッセージの整理
-			$key=time();
-			$labor[$key]=$message->id;
-			//保存データの整理
-			$data['user_id']=$inputs['recipient_id'];
-			$data['message']=serialize($labor);
-			//データ登録
-				Labor::create($data);
+			$work=Work::where('user_id',$inputs['recipient_id'])->first();
+			//旧メッセージ
+			$old=isset($work->message) ? unserialize($work->message) : array();
+			//新メッセージ
+			$new=array($message->id);
+			//配列の併合
+			$merge=isset($old) ? array_merge($old,$new) : $new;
+			//登録データの整理
+			$work->message=serialize($merge);
+			$work->save();
 		}
-		
-		
-		
-	
 	//トップページへ移動
-	return Redirect::to('message/index');
+	return Redirect::to('message/unread');
+	}
+/*
+|------------------------------------
+| 未読メッセージ
+|------------------------------------
+*/
+	public function getUnread($id=null,$key=null){
+		//ログインユーザーのWorkオブジェクトを取得
+		$work=Work::where('user_id',Auth::user()->id)->first();
+		//未読メッセージがなければ
+		if($work->message == null){
+			return '未読メッセージはありません';
+		}
+		//指定IDが未読メッセージの中にあれば
+		if(isset($id) and in_array($id,unserialize($work->message))){
+			//return $key;
+				//未読メッセージIDの配列を取得
+				$unread=unserialize($work->message);
+					//未読メッセージを削除
+					unset($unread[$key]);
+				$message=serialize($unread);
+				$work->message=$message;
+				$work->save();
+		}
+		//未読メッセージの配列取得
+		$unread=isset($work) ? unserialize($work->message) : null;
+		//return var_dump($unread);
+		//配列の数だけオブジェクトを取得
+		foreach($unread as $key=>$value):
+			$data['message'][]=Message::find($value);
+		endforeach;
+		//return var_dump($data['message']);
+		return View::make('message/unread',$data);
 	}
 }
